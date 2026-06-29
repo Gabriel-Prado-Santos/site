c7b5c55056579adfd156875e6c5124088c21212d
Gabriel-Prado-Santos <167914375+Gabriel-Prado-Santos@users.noreply.github.com>
2026-06-29 02:22:13 -0300

    controle de fluxo/laços p.t3

--- JavaScript/aula21.js
+++ JavaScript/aula21.js
@@ -1,14 +1,71 @@
 const carros = ["Uno", "Fusca", "Escort", "Gol"];
-
+let dados = ""
+let i = 0
 const conteudo = document.getElementById("conteudo");
+// função criada na aula 22
+function criarSecao(titulo, dadosF){
+    let secao = document.createElement("div");
+    secao.innerHTML = "<h2>" + titulo + "</h2>" + dadosF;
+    conteudo.appendChild(secao);
+}
 
-let dados = "";
-let i = 0;
+dados = "";
+i = 0;
 while( i < 4){
     dados += "<p>" + carros[i] + "</p>";
     i++;
 }
 
-const secao = document.createElement("div");
-secao.innerHTML = "<h2>Loop while</h2>" + dados;
-conteudo.appendChild(secao);
+criarSecao("Loop While", dados);
+
+dados = "";
+i = 0;
+while( i < carros.length){
+    dados += "<p>" + carros[i] + "</p>";
+    i++;
+}
+
+criarSecao("Loop while melhorado", dados);
+
+dados = "";
+i = 0 
+do {
+    dados += "<p>" + carros[i] + "</p>";
+    i++;
+} while(i < carros.length);
+criarSecao("Loop do-while", dados);
+
+//laço for
+
+dados = "";
+for(i = 0; i < carros.length; i++){
+    dados += "<p>" + carros[i] + "</p>";
+}
+criarSecao("Loop whilemelhorado", dados);
+
+dados = "";
+
+for(let carro of carros){
+    dados += "<p>" + carro + "</p>";
+}
+criarSecao("Loop For of", dados);
+
+// objeto
+let carro1 = {marca: "Ford", modelo: "Ka", ano: "2015"};
+let carro2 = {marca: "Fiat", modelo: "Uno", ano: "2000"};
+
+let carros2 = [];
+carros2.push(carro1);
+carros2.push(carro2);
+
+/* agora temos uma matriz*/
+
+// for in
+
+for(let carro of carros2){
+    let propriedades = ""
+    for(let prop in carro){
+        propriedades += carro[prop] + " | ";
+    }
+    dados+= "<p>" + propriedades + "</p>";
+}
